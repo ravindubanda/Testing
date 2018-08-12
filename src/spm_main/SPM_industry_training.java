894202ce6eb56b8cc23f0bcffa18f20a57abc6f1
unknown <sithara.liyanaarachchi@gmail.com>
2018-08-12 13:32:09 +0530

    init

---
+++ src/spm_main/SPM_industry_training.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package spm_main;
+
+/**
+ *
+ * @author Asus
+ */
+public class SPM_industry_training {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
